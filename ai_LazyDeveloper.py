04aefe32848f505cc2e29da69fc4c4c87c0eec08
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2024-01-21 00:04:02 +0400

    Add files via upload

---
+++ ai_LazyDeveloper.py
@@ -0,0 +1,40 @@
+#
+#         i am NOT INTRESTED IN BEING YOUR SECOND FATHER... SO DON'T REMOVE MY CREDIT...
+#                                       @LazyDeveloperr
+# 
+# 
+# 
+# 
+#
+#
+#
+#
+#
+#
+#
+#                                                ...PRESENTING...
+#                                  A R T I F i C I A L  -  I N T E L i G E N C E
+#                                              .in  Auto-Filter-Bot.
+#                                                 @ngrocker
+#
+#                                       
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#         this line is for copy-pasters...
+#         ...while you are removing my credit and calling yourself a developerr... 
+#         _____ just imagine, At that time i am fucking your mom and sis at same time, harder & too harder...
+#
+# Credit @ngrocker.
+# Please Don't remove credit.
+# Born to make history @ngrocker !
+# Thank you ngrocker for helping us in this Journey
+# 🥰  Thank you for giving me credit @LazyDeveloperr  🥰
+# for any error please contact me -> telegram @ngrocker or insta @adarshmedias
